87f87d0cd7af693832d8ad337377292990140596
Dannie Vinther <davi@kea.dk>
2026-02-17 15:11:39 +0100

    fix: update base path in astro.config.mjs for correct deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://dannievinther.github.io/",
-  base: "./uden-netlify",
+  base: "/uden-netlify",
 });
