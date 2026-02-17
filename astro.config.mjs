c54bb50a50d1f3ce47f66168d56675666063b2e3
Dannie Vinther <davi@kea.dk>
2026-02-17 14:50:37 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dannievinther.github.io/uden-netlify/",
+  site: "https://dannievinther.github.io/",
 });
